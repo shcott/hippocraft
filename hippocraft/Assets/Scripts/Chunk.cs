ae910da32520066b7ad19a308f328e9aaea5089e
Scott <scott.huang.21@outlook.com>
2017-05-22 02:27:47 -0500

    Add ChunkMesh; fix mesh collider not working

--- hippocraft/Assets/Scripts/Chunk.cs
+++ hippocraft/Assets/Scripts/Chunk.cs
@@ -4,39 +4,24 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour {
 
-	public static readonly int CHUNK_SIZE = 16;
+	public static readonly int CHUNK_SIZE = 10;
 	public static readonly int CHUNK_HEIGHT = 128;
 
 	private int chunkX;
 	private int chunkZ;
 	private int[,,] tiles;
-
-	private List<Vector3> vertices;
-	private List<int> triangles;
+	private ChunkMesh chunkMesh;
 
 	/**
 	 * Initializes the chunk's data. Must be manually called.
 	 */
 	public void InitChunk(int x, int z) {
 		tiles = new int[CHUNK_SIZE, CHUNK_HEIGHT, CHUNK_SIZE];
-		//InitTiles();
-
-		vertices = new List<Vector3>();
-		triangles = new List<int>();
+		chunkMesh = new ChunkMesh();
 
 		SetCoords(x, z);
 		GenerateDefaultTiles();
 	}
-
-	/*private void InitTiles() {
-		for(int x = 0; x < CHUNK_SIZE; x++) {
-			for(int y = 0; y < CHUNK_HEIGHT; y++) {
-				for(int z = 0; z < CHUNK_SIZE; z++) {
-					tiles[x, y, z] = 0;
-				}
-			}
-		}
-	}*/
 
 	public void SetCoords(int x, int z) {
 		chunkX = x;
@@ -96,8 +81,7 @@ public class Chunk : MonoBehaviour {
 	 * only called initially or when a tile has been changed.
 	 */
 	public void ReconstructMesh() {
-		vertices.Clear();
-		triangles.Clear();
+		chunkMesh.Clear();
 
 		int startIndex = 0;
 		for(int x = 0; x < CHUNK_SIZE; x++) {
@@ -106,129 +90,23 @@ public class Chunk : MonoBehaviour {
 					if(tiles[x, y, z] == 0)
 						continue;
 					if(!MapGenerator.IsTileBlock(x+1, y  , z  ))
-						AddRightFace(x, y, z, ref startIndex);
+						chunkMesh.AddRightFace(x, y, z, ref startIndex);
 					if(!MapGenerator.IsTileBlock(x-1, y  , z  ))
-						AddLeftFace(x, y, z, ref startIndex);
+						chunkMesh.AddLeftFace(x, y, z, ref startIndex);
 					if(!MapGenerator.IsTileBlock(x  , y+1, z  ))
-						AddTopFace(x, y, z, ref startIndex);
+						chunkMesh.AddTopFace(x, y, z, ref startIndex);
 					if(!MapGenerator.IsTileBlock(x  , y-1, z  ))
-						AddBottomFace(x, y, z, ref startIndex);
+						chunkMesh.AddBottomFace(x, y, z, ref startIndex);
 					if(!MapGenerator.IsTileBlock(x  , y  , z+1))
-						AddBackFace(x, y, z, ref startIndex);
+						chunkMesh.AddBackFace(x, y, z, ref startIndex);
 					if(!MapGenerator.IsTileBlock(x  , y  , z-1))
-						AddFrontFace(x, y, z, ref startIndex);
+						chunkMesh.AddFrontFace(x, y, z, ref startIndex);
 				}
 			}
 		}
 
-		Mesh mesh = new Mesh();
+		Mesh mesh = chunkMesh.CreateMesh();
 		GetComponent<MeshFilter>().mesh = mesh;
-		mesh.vertices = vertices.ToArray();
-		mesh.triangles = triangles.ToArray();
-		mesh.RecalculateNormals();
-	}
-
-
-	// TODO: Remove duplicate vertices?
-	private void AddRightFace(int x, int y, int z, ref int startIndex) {
-		vertices.Add(new Vector3(x+1, y  , z  ));
-		vertices.Add(new Vector3(x+1, y  , z+1));
-		vertices.Add(new Vector3(x+1, y+1, z+1));
-		vertices.Add(new Vector3(x+1, y+1, z  ));
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 2);
-		triangles.Add(startIndex + 1);
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 3);
-		triangles.Add(startIndex + 2);
-
-		startIndex += 4;
-	}
-
-	private void AddLeftFace(int x, int y, int z, ref int startIndex) {
-		vertices.Add(new Vector3(x, y  , z+1));
-		vertices.Add(new Vector3(x, y  , z  ));
-		vertices.Add(new Vector3(x, y+1, z  ));
-		vertices.Add(new Vector3(x, y+1, z+1));
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 2);
-		triangles.Add(startIndex + 1);
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 3);
-		triangles.Add(startIndex + 2);
-
-		startIndex += 4;
-	}
-
-	private void AddTopFace(int x, int y, int z, ref int startIndex) {
-		vertices.Add(new Vector3(x  , y+1, z  ));
-		vertices.Add(new Vector3(x+1, y+1, z  ));
-		vertices.Add(new Vector3(x+1, y+1, z+1));
-		vertices.Add(new Vector3(x  , y+1, z+1));
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 2);
-		triangles.Add(startIndex + 1);
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 3);
-		triangles.Add(startIndex + 2);
-
-		startIndex += 4;
-	}
-
-	private void AddBottomFace(int x, int y, int z, ref int startIndex) {
-		vertices.Add(new Vector3(x  , y, z+1));
-		vertices.Add(new Vector3(x+1, y, z+1));
-		vertices.Add(new Vector3(x+1, y, z  ));
-		vertices.Add(new Vector3(x  , y, z  ));
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 2);
-		triangles.Add(startIndex + 1);
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 3);
-		triangles.Add(startIndex + 2);
-
-		startIndex += 4;
-	}
-
-	private void AddFrontFace(int x, int y, int z, ref int startIndex) {
-		vertices.Add(new Vector3(x  , y  , z));
-		vertices.Add(new Vector3(x+1, y  , z));
-		vertices.Add(new Vector3(x+1, y+1, z));
-		vertices.Add(new Vector3(x  , y+1, z));
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 2);
-		triangles.Add(startIndex + 1);
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 3);
-		triangles.Add(startIndex + 2);
-
-		startIndex += 4;
-	}
-
-	private void AddBackFace(int x, int y, int z, ref int startIndex) {
-		vertices.Add(new Vector3(x+1, y  , z+1));
-		vertices.Add(new Vector3(x  , y  , z+1));
-		vertices.Add(new Vector3(x  , y+1, z+1));
-		vertices.Add(new Vector3(x+1, y+1, z+1));
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 2);
-		triangles.Add(startIndex + 1);
-
-		triangles.Add(startIndex + 0);
-		triangles.Add(startIndex + 3);
-		triangles.Add(startIndex + 2);
-
-		startIndex += 4;
+		GetComponent<MeshCollider>().sharedMesh = mesh;
 	}
 }
---
+++ hippocraft/Assets/Scripts/ChunkMesh.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * A class containing all properties regarding a chunk's mesh for the sake of organization.
+ */
+public class ChunkMesh {
+
+	private List<Vector3> vertices;
+	private List<int> triangles;
+
+	public ChunkMesh() {
+		vertices = new List<Vector3>();
+		triangles = new List<int>();
+	}
+
+	public void Clear() {
+		vertices.Clear();
+		triangles.Clear();
+	}
+
+	public Mesh CreateMesh() {
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.RecalculateNormals();
+		return mesh;
+	}
+
+	// TODO: Remove duplicate vertices?
+	public void AddRightFace(int x, int y, int z, ref int startIndex) {
+		vertices.Add(new Vector3(x+1, y  , z  ));
+		vertices.Add(new Vector3(x+1, y  , z+1));
+		vertices.Add(new Vector3(x+1, y+1, z+1));
+		vertices.Add(new Vector3(x+1, y+1, z  ));
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 2);
+		triangles.Add(startIndex + 1);
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 3);
+		triangles.Add(startIndex + 2);
+
+		startIndex += 4;
+	}
+
+	public void AddLeftFace(int x, int y, int z, ref int startIndex) {
+		vertices.Add(new Vector3(x, y  , z+1));
+		vertices.Add(new Vector3(x, y  , z  ));
+		vertices.Add(new Vector3(x, y+1, z  ));
+		vertices.Add(new Vector3(x, y+1, z+1));
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 2);
+		triangles.Add(startIndex + 1);
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 3);
+		triangles.Add(startIndex + 2);
+
+		startIndex += 4;
+	}
+
+	public void AddTopFace(int x, int y, int z, ref int startIndex) {
+		vertices.Add(new Vector3(x  , y+1, z  ));
+		vertices.Add(new Vector3(x+1, y+1, z  ));
+		vertices.Add(new Vector3(x+1, y+1, z+1));
+		vertices.Add(new Vector3(x  , y+1, z+1));
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 2);
+		triangles.Add(startIndex + 1);
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 3);
+		triangles.Add(startIndex + 2);
+
+		startIndex += 4;
+	}
+
+	public void AddBottomFace(int x, int y, int z, ref int startIndex) {
+		vertices.Add(new Vector3(x  , y, z+1));
+		vertices.Add(new Vector3(x+1, y, z+1));
+		vertices.Add(new Vector3(x+1, y, z  ));
+		vertices.Add(new Vector3(x  , y, z  ));
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 2);
+		triangles.Add(startIndex + 1);
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 3);
+		triangles.Add(startIndex + 2);
+
+		startIndex += 4;
+	}
+
+	public void AddFrontFace(int x, int y, int z, ref int startIndex) {
+		vertices.Add(new Vector3(x  , y  , z));
+		vertices.Add(new Vector3(x+1, y  , z));
+		vertices.Add(new Vector3(x+1, y+1, z));
+		vertices.Add(new Vector3(x  , y+1, z));
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 2);
+		triangles.Add(startIndex + 1);
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 3);
+		triangles.Add(startIndex + 2);
+
+		startIndex += 4;
+	}
+
+	public void AddBackFace(int x, int y, int z, ref int startIndex) {
+		vertices.Add(new Vector3(x+1, y  , z+1));
+		vertices.Add(new Vector3(x  , y  , z+1));
+		vertices.Add(new Vector3(x  , y+1, z+1));
+		vertices.Add(new Vector3(x+1, y+1, z+1));
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 2);
+		triangles.Add(startIndex + 1);
+
+		triangles.Add(startIndex + 0);
+		triangles.Add(startIndex + 3);
+		triangles.Add(startIndex + 2);
+
+		startIndex += 4;
+	}
+}
